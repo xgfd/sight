bbe56f8461f233c1137f6936763fdea19c1d1776
Xin Wang <x.wang@sigmasquares.com>
2021-02-21 01:23:46 +0800

    update dev constant to work on both mac and win; print all constants in console; add toggle dev tools for win release

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import os from 'os';
 const VISION =
   process.env.NODE_ENV !== 'development'
     ? path.join(process.resourcesPath, 'vision')
-    : path.join(process.resourcesPath, '../../../../../../vision');
+    : path.join(__dirname, '..', 'vision');
 
 const BUILTIN = path.join(VISION, 'builtin');
 const CUSTOM = path.join(VISION, 'custom');
@@ -18,5 +18,7 @@ const IMAGE_CACHE = path.join(APP_DATA, 'cache');
 fs.mkdir(IMAGE_CACHE, { recursive: true }, (err) => {
   if (err) console.error(err);
 });
+
+console.log({ VISION, BUILTIN, TEMPLATES, CUSTOM, IMAGE_CACHE });
 
 export { VISION, BUILTIN, TEMPLATES, CUSTOM, IMAGE_CACHE };
