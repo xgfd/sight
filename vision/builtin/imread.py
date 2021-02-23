9a527d3c729b8d79e4bb35a4968cd26bea874ac1
Xin Wang <x.wang@sigmasquares.com>
2021-02-23 17:03:22 +0000

    fix imread to support utf8 chars on windows

--- vision/builtin/imread.py
+++ vision/builtin/imread.py
@@ -1,7 +1,9 @@
-from pathlib import Path
 import cv2
+import numpy as np
 
 
 def main(filepath: str, flags: int):
-    image = cv2.imread(str(Path(filepath).resolve()), flags=flags)
+    # use fromfile since imread doesn't handle utf8 well
+    arr = np.fromfile(filepath, dtype=np.uint8)
+    image = cv2.imdecode(arr, flags=flags)
     return image
