6c4a81b4baa5b9d539476eb318559c1bdea22dde
Xin Wang <x.wang@sigmasquares.com>
2021-04-19 22:24:06 +0100

    separate css from ZoomViewer

--- src/components/image/image-internal/ZoomViewer.tsx
+++ src/components/image/image-internal/ZoomViewer.tsx
@@ -5,6 +5,7 @@ import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import { Zoom } from '@visx/zoom';
 import { ProvidedZoom } from '@visx/zoom/lib/types';
 import React, { useState } from 'react';
+import styles from './ZoomViewer.module.css';
 
 const bg = '#0a0a0a';
 
@@ -34,121 +35,88 @@ export default function ZoomViewer({
   return (
     <ParentSize>
       {({ width, height }) => (
-        <div className={className}>
-          <Zoom
-            style={{ pointerEvents: 'auto' }}
-            width={width}
-            height={height}
-            scaleXMin={0.8}
-            scaleXMax={100}
-            scaleYMin={0.8}
-            scaleYMax={100}
-            transformMatrix={initialTransform}
-          >
-            {(zoom) => {
-              exposeZoom(zoom);
-              return (
-                <div className="relative">
-                  <svg
+        <Zoom
+          className={className}
+          style={{ pointerEvents: 'auto' }}
+          width={width}
+          height={height}
+          scaleXMin={0.8}
+          scaleXMax={100}
+          scaleYMin={0.8}
+          scaleYMax={100}
+          transformMatrix={initialTransform}
+        >
+          {(zoom) => {
+            exposeZoom(zoom);
+            return (
+              <div>
+                <svg
+                  width={width}
+                  height={height}
+                  style={{ cursor: zoom.isDragging ? 'grabbing' : 'grab' }}
+                >
+                  <RectClipPath id="zoom-clip" width={width} height={height} />
+                  <rect width={width} height={height} rx={0} fill={bg} />
+                  <image
                     width={width}
                     height={height}
-                    style={{ cursor: zoom.isDragging ? 'grabbing' : 'grab' }}
-                  >
-                    <RectClipPath
-                      id="zoom-clip"
-                      width={width}
-                      height={height}
-                    />
-                    <rect width={width} height={height} rx={0} fill={bg} />
-                    <image
-                      width={width}
-                      height={height}
-                      imageRendering="pixelated"
-                      transform={zoom.toString()}
-                      href={imgRef}
-                    />
-                    <rect
-                      width={width}
-                      height={height}
-                      rx={0}
-                      fill="transparent"
-                      onTouchStart={zoom.dragStart}
-                      onTouchMove={zoom.dragMove}
-                      onTouchEnd={zoom.dragEnd}
-                      onMouseDown={zoom.dragStart}
-                      onMouseMove={zoom.dragMove}
-                      onMouseUp={zoom.dragEnd}
-                      onMouseLeave={() => {
-                        if (zoom.isDragging) zoom.dragEnd();
-                      }}
-                      onDoubleClick={(event) => {
-                        const point = localPoint(event) || { x: 0, y: 0 };
-                        zoom.scale({ scaleX: 1.1, scaleY: 1.1, point });
-                      }}
-                    />
-                    {showMiniMap && (
-                      <g
-                        clipPath="url(#zoom-clip)"
-                        transform={`
-                    scale(0.25)
-                    translate(${width * 4 - width - 60}, ${
-                          height * 4 - height - 60
-                        })
-                  `}
-                      >
-                        <rect width={width} height={height} fill="#1a1a1a" />
-                        <image width={width} height={height} href={imgRef} />
-                        <rect
-                          width={width}
-                          height={height}
-                          fill="white"
-                          fillOpacity={0.2}
-                          stroke="white"
-                          strokeWidth={4}
-                          transform={zoom.toStringInvert()}
-                        />
-                      </g>
-                    )}
-                  </svg>
-                  <div className="mini-map">
-                    <button
-                      type="button"
-                      className="btn btn-lg"
-                      onClick={() => setShowMiniMap(!showMiniMap)}
+                    imageRendering="pixelated"
+                    transform={zoom.toString()}
+                    href={imgRef}
+                  />
+                  <rect
+                    width={width}
+                    height={height}
+                    rx={0}
+                    fill="transparent"
+                    onTouchStart={zoom.dragStart}
+                    onTouchMove={zoom.dragMove}
+                    onTouchEnd={zoom.dragEnd}
+                    onMouseDown={zoom.dragStart}
+                    onMouseMove={zoom.dragMove}
+                    onMouseUp={zoom.dragEnd}
+                    onMouseLeave={() => {
+                      if (zoom.isDragging) zoom.dragEnd();
+                    }}
+                    onDoubleClick={(event) => {
+                      const point = localPoint(event) || { x: 0, y: 0 };
+                      zoom.scale({ scaleX: 1.1, scaleY: 1.1, point });
+                    }}
+                  />
+                  {showMiniMap && (
+                    <g
+                      clipPath="url(#zoom-clip)"
+                      transform={`scale(0.25) translate(${
+                        width * 4 - width - 60
+                      }, ${height * 4 - height - 60})`}
                     >
-                      {showMiniMap ? 'Hide' : 'Show'} Mini Map
-                    </button>
-                  </div>
+                      <rect width={width} height={height} fill="#1a1a1a" />
+                      <image width={width} height={height} href={imgRef} />
+                      <rect
+                        width={width}
+                        height={height}
+                        fill="white"
+                        fillOpacity={0.2}
+                        stroke="white"
+                        strokeWidth={4}
+                        transform={zoom.toStringInvert()}
+                      />
+                    </g>
+                  )}
+                </svg>
+                <div className={styles['mini-map']}>
+                  <button
+                    type="button"
+                    className={styles.btn}
+                    onClick={() => setShowMiniMap(!showMiniMap)}
+                  >
+                    {showMiniMap ? 'Hide' : 'Show'} Mini Map
+                  </button>
                 </div>
-              );
-            }}
-          </Zoom>
-          <style>{`
-        .btn {
-          margin: 0;
-          text-align: center;
-          border: none;
-          background: #2f2f2f;
-          color: #888;
-          padding: 0 4px;
-          border-top: 1px solid #0a0a0a;
-          outline:none;
-        }
-        .btn-lg {
-          font-size: 12px;
-          line-height: 1;
-          padding: 4px;
-        }
-        .mini-map {
-          position: absolute;
-          bottom: 25px;
-          right: 15px;
-          display: flex;
-          flex-direction: column;
-          align-items: flex-end;
-        }
-      `}</style>
-        </div>
+              </div>
+            );
+          }}
+        </Zoom>
       )}
     </ParentSize>
   );
