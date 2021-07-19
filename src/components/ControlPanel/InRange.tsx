6358cd8cd1b2a658ff9172d29caeb47da5dce554
Xin Wang <x.wang@sigmasquares.com>
2021-07-19 21:27:40 +0100

    fix inRange control panel

--- src/components/ControlPanel/InRange.tsx
+++ src/components/ControlPanel/InRange.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Slider } from 'antd';
 import ControlsBase from './ControlsBase';
 
 export default class InvertControls extends ControlsBase {
-  static defaultValues = [];
+  static defaultValues = [[50, 150] as [number, number]];
 
   render() {
-    const { name } = this.state;
+    const { name, args } = this.state;
 
     return (
       <>
         <h2>{name}</h2>
+        <h4>Thresholds</h4>
+        <Slider
+          marks={{ 0: '0', 127: '127', 255: '255' }}
+          range
+          max={255}
+          value={args[0]}
+          onChange={(value: [number, number]) => this.updateArgs(0, value)}
+        />
       </>
     );
   }
