e5a756d604e559f179778a78acf41f25b9ba655d
Xin Wang <x.wang@sigmasquares.com>
2021-08-16 00:12:03 +0100

    fix dtype issue in normalize

--- vision/builtin/normalize.py
+++ vision/builtin/normalize.py
@@ -2,7 +2,6 @@ import cv2
 
 
 def main(image: object, alpha, beta, norm_type, dtype=-1):
-    cv2.normalize(
-        image, image, alpha=alpha, beta=beta, norm_type=norm_type, dtype=dtype
+    return cv2.normalize(
+        image, None, alpha=alpha, beta=beta, norm_type=norm_type, dtype=dtype
     )
-    return image
