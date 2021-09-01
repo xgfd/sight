8266c50558dc6753f64f24318b280d050a4419ca
Xin Wang <x.wang@sigmasquares.com>
2021-09-01 18:08:57 +0100

    increase convertScaleAbs scale

--- src/components/ControlPanel/ConvertScaleAbs.tsx
+++ src/components/ControlPanel/ConvertScaleAbs.tsx
@@ -13,7 +13,7 @@ export default class ConvertScaleAbsControls extends ControlsBase {
         <h2>{name}</h2>
         <h4>Alpha (contrast)</h4>
         <Slider
-          max={50}
+          max={255}
           step={0.1}
           value={args[0]}
           onChange={(value: number) => this.updateArgs(0, value)}
