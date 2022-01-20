a5cfe2dff33080b7333837a884ef0eb2327e6358
Xin Wang <x.wang@sigmasquares.com>
2022-01-20 14:59:20 +0000

    fix threshold Otsu and triangle type options

--- src/components/ControlPanel/Threshold.tsx
+++ src/components/ControlPanel/Threshold.tsx
@@ -1,8 +1,29 @@
-import { InputNumber, Select, Slider } from 'antd';
+import { InputNumber, Select, Slider, Switch } from 'antd';
 import React from 'react';
 import ControlsBase from './ControlsBase';
 
 const { Option } = Select;
+
+// bit mask for Otsu threshholding
+const OtsuMask = 8;
+
+// bit mask for Triangle threshholding
+const TriangleMask = 16;
+
+function checkBit(val: number, mask: number) {
+  /* eslint-disable-next-line no-bitwise */
+  return (val & mask) !== 0;
+}
+
+function setBit(val: number, mask: number) {
+  /* eslint-disable-next-line no-bitwise */
+  return val | mask;
+}
+
+function clearBit(val: number, mask: number) {
+  /* eslint-disable-next-line no-bitwise */
+  return val & ~mask;
+}
 
 export default class ThresholdControls extends ControlsBase {
   static defaultValues = [127, 255, 0];
@@ -29,7 +50,8 @@ export default class ThresholdControls extends ControlsBase {
         />
         <h4>Threshold type</h4>
         <Select
-          value={args[2]}
+          /* eslint-disable-next-line no-bitwise */
+          value={args[2] & 7}
           showSearch
           optionFilterProp="children"
           style={{ width: '100%' }}
@@ -40,10 +62,37 @@ export default class ThresholdControls extends ControlsBase {
           <Option value={2}>TRUNC</Option>
           <Option value={3}>TOZERO</Option>
           <Option value={4}>TOZERO_INV</Option>
-          <Option value={7}>THRESH_MASK</Option>
-          <Option value={8}>THRESH_OTSU</Option>
-          <Option value={16}>TRIANGLE</Option>
         </Select>
+        <h4>Use Otsu threshholding</h4>
+        <Switch
+          checked={checkBit(args[2], OtsuMask)}
+          onChange={(checked) => {
+            let threshType = args[2];
+            if (checked) {
+              // set Otsu mask and clear triangle mask
+              threshType = setBit(threshType, OtsuMask);
+              threshType = clearBit(threshType, TriangleMask);
+            } else {
+              threshType = clearBit(threshType, OtsuMask);
+            }
+            this.updateArgs(2, threshType);
+          }}
+        />
+        <h4>Use triangle threshholding</h4>
+        <Switch
+          checked={checkBit(args[2], TriangleMask)}
+          onChange={(checked) => {
+            let threshType = args[2];
+            if (checked) {
+              // set triangle mask and clear Otsu mask
+              threshType = setBit(threshType, TriangleMask);
+              threshType = clearBit(threshType, OtsuMask);
+            } else {
+              threshType = clearBit(threshType, TriangleMask);
+            }
+            this.updateArgs(2, threshType);
+          }}
+        />
       </>
     );
   }
